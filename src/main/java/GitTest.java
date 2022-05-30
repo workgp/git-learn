c080deaeb360a8b0d6f9455ea91cbfe5d2b6d714
guanpeng <guanpeng@gp.com>
2022-05-30 12:23:36 +0800

    first commit testGit

---
+++ src/main/java/GitTest.java
@@ -0,0 +1,12 @@
+/**
+ * @ClassName: GitTest
+ * @Description:
+ * @Author
+ * @Date 2022/5/30
+ * @Version
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
