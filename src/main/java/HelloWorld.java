a871b9e24801fb47ae4dabf8093cf35a0a8a8325
guanpeng <guanpeng@gp.com>
2022-05-31 18:19:20 +0800

    hello world

---
+++ src/main/java/HelloWorld.java
@@ -0,0 +1,14 @@
+
+
+/**
+ * @ClassName: HelloWorld
+ * @Description:
+ * @Author
+ * @Date 2022/5/31
+ * @Version
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
