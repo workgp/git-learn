a3d1eabe8b8bb59080c410259f28bfeadc784fbc
guanpeng <guanpeng@gp.com>
2022-05-31 18:24:23 +0800

    Test01

---
+++ src/main/java/Test01.java
@@ -0,0 +1,12 @@
+/**
+ * @ClassName: Test01
+ * @Description:
+ * @Author
+ * @Date 2022/5/31
+ * @Version
+ */
+public class Test01 {
+    public static void main(String[] args) {
+        System.out.println("你是谁");
+    }
+}
